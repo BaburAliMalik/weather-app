b942b6a511a00b401ceca7f186d05aa688b7586d
baburali <baburali1910@gmail.com>
2024-09-08 18:37:00 -0700

    add changes

---
+++ src/components/Weather.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const api ={
+    key : 'abcb7ba1dd6171022e425bc87b0ce71b',
+    base : 'https://api.openweathermap.org/data/2.5/',
+}
+const Weather = () => {
+  return (
+    <div>Weather</div>
+  )
+}
+
+export default Weather
